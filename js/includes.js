bd56bf59596c96e3a3c4cbfcae97391b7d841572
Leandro Gabriel <leandro@bigiogabriel.com>
2022-09-06 22:40:20 +0100

    Add header, asides and footer to an include file

---
+++ js/includes.js
@@ -0,0 +1,37 @@
+fetch("/header.html")
+.then((response) => {
+    return response.text();
+})
+.then((data) => {
+    document.querySelector("header").innerHTML = data;
+})
+.then((data) => {
+    let classLink = window.location.pathname.split("/")[1];
+    let myLink = document.querySelector(
+        `[href='/${classLink}/']`
+    );
+    myLink.classList.add("active");
+});
+
+fetch("/footer.html")
+.then((response) => {
+    return response.text();
+})
+.then((data) => {
+    document.querySelector("footer").innerHTML = data;
+});
+
+fetch("/asideClasses.html")
+.then((response) => {
+    return response.text();
+})
+.then((data) => {
+    document.querySelector("#classes-nav").innerHTML = data;
+})
+.then((data) => {
+    let classLink = window.location.pathname;
+    let myLink = document.querySelector(
+        `[href='${classLink}']`
+    );
+    myLink.classList.add("active");
+});
