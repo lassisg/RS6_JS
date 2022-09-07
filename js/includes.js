b9e22d031182fdb857ae5be8b20ea4106666c2f7
Leandro Gabriel <leandro@bigiogabriel.com>
2022-09-07 01:23:36 +0100

    Adds exercises asside to includes file

--- js/includes.js
+++ js/includes.js
@@ -1,37 +1,52 @@
 fetch("/header.html")
-.then((response) => {
-    return response.text();
-})
-.then((data) => {
-    document.querySelector("header").innerHTML = data;
-})
-.then((data) => {
-    let classLink = window.location.pathname.split("/")[1];
-    let myLink = document.querySelector(
-        `[href='/${classLink}/']`
-    );
-    myLink.classList.add("active");
-});
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        document.querySelector("header").innerHTML = data;
+    })
+    .then((data) => {
+        let classLink = window.location.pathname.split("/")[1];
+        let myLink = document.querySelector(
+            `[href='/${classLink}/']`
+        );
+        myLink.classList.add("active");
+    });
 
 fetch("/footer.html")
-.then((response) => {
-    return response.text();
-})
-.then((data) => {
-    document.querySelector("footer").innerHTML = data;
-});
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        document.querySelector("footer").innerHTML = data;
+    });
 
 fetch("/asideClasses.html")
-.then((response) => {
-    return response.text();
-})
-.then((data) => {
-    document.querySelector("#classes-nav").innerHTML = data;
-})
-.then((data) => {
-    let classLink = window.location.pathname;
-    let myLink = document.querySelector(
-        `[href='${classLink}']`
-    );
-    myLink.classList.add("active");
-});
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        document.querySelector("#classes-nav").innerHTML = data;
+    })
+    .then((data) => {
+        let classLink = window.location.pathname;
+        let myLink = document.querySelector(
+            `[href='${classLink}']`
+        );
+        myLink.classList.add("active");
+    });
+
+fetch("/asideExercises.html")
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        document.querySelector("#exercises-nav").innerHTML = data;
+    })
+    .then((data) => {
+        let classLink = window.location.pathname;
+        let myLink = document.querySelector(
+            `[href='${classLink}']`
+        );
+        myLink.classList.add("active");
+    });
